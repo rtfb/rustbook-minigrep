2e245613a58fb3bdf96a47cdd6da6ca6f605df0c
Vytautas Saltenis <vytas@rtfb.lt>
2019-12-29 20:50:30 +0200

    Call search() from run()

--- src/lib.rs
+++ src/lib.rs
@@ -19,6 +19,9 @@ impl Config {
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.filename)?;
+    for line in search(&config.query, &contents) {
+        println!("{}", line)
+    }
     Ok(())
 }
 
